d65e3d90038437227285e8d2b8382783dd3148c2
nahuelflores1 <nahuelflores2018@hotmail.com>
2023-05-30 10:04:15 -0300

    js

---
+++ JS 15-05/app.js
@@ -0,0 +1,13 @@
+var nave = {
+    tipo: "Espacial",
+    pais: "País de origen de la nave",
+    cantidadTripulantes: 5,
+    tripulantes: ["Tripulante 1", "Tripulante 2", "Tripulante 3", "Tripulante 4", "Tripulante 5"],
+    estado: "Usada",
+    
+  };
+
+function despegar () {
+    console.log("¡Todo listo para el despegue!  no... ¡algo se rompió!");
+    alert("¡BOOOM!");
+}
