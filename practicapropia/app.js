16b6b4a1ac49131c73af31902f3945736e6fcafc
nahuelflores1 <nahuelflores2018@hotmail.com>
2023-05-31 09:36:55 -0300

    js practica

---
+++ practicapropia/app.js
@@ -0,0 +1,13 @@
+let notas = prompt("Ingrese su nota");
+
+function evaluator(notas){
+    if(notas <= 0  || notas > 10){
+        alert("Debe ingresar un valor entre 0 y 10");
+    }
+}
+
+evaluator(notas);
+  
+
+
+
